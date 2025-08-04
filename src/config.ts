39812a88c558a44a12aaa0aa1c439fbac662f1f1
Emmanuel Kiriinya <emmanuelkiriinya4229@gmail.com>
2025-08-04 12:35:12 +0300

    Revert "frontend"

--- src/config.ts
+++
@@ -1,5 +0,0 @@
-// src/config.ts
-export const API_URL = "http://127.0.0.1:8000"
-  import.meta.env.MODE === "development"
-    ? "http://127.0.0.1:8000" // local FastAPI
-    : "https://facial-emotions-prediction.onrender.com"; // Render backend
